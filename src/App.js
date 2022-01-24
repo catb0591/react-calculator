850a15beb8478e1e964950231e05b7e56b9d144f
Cat <catb0591@gmail.com>
2022-01-23 22:09:20 -0500

    Skeleton of Calculator, Error and Input components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Title from './components/title';
+import Input from './components/input';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+        <form onsubmit>
+          <Input/>
+        </form>
     </div>
   );
 }
