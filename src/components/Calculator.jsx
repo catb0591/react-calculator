8057f6058425f00bba36d762b5db236c7cd85a92
Cat <catb0591@gmail.com>
2022-01-24 00:54:34 -0500

    calculator functioning with components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from "react";
+/* eslint-disable no-eval */
+import React, {useState} from "react";
 import Error from "./Error";
 import Input from "./Input";
 import SelectBox from "./SelectBox";
@@ -12,21 +13,27 @@ const Calculator = () => {
     const [error, setError] = useState('')
     const [answer, setAnswer] = useState()
 
+    const handleError = () => {
+           if (isNaN(numberOne) || isNaN(numberTwo) || numberOne === '' || numberTwo === '') {
+               setError('Please provide a valid number for both operands')
+           } else { setError('') }
+       }
 
-   useEffect(()=> {
-       if (isNaN(numberOne) || isNaN(numberTwo)) {
-           setError('Please provide a valid number for both operands')
-       } else {setError('')}
-   }, [numberOne, numberTwo])
-    
+   const handleAnswer = () => {
+       handleError()
+       if (isNaN(numberOne) || isNaN(numberTwo) || numberOne === '' || numberTwo === ''){
+       setAnswer('')
+       } else { setAnswer(eval(numberOne + operator + numberTwo))}
+   }
+
     return(
         <div>
             <Title/>
             <Input type="text" setValue={setValueOne} number={numberOne}/>
             <SelectBox setOperator={setOperator} operator={operator}/>
             <Input type="text" setValue={setValueTwo} number={numberTwo} />
-            <input type="button" value="=" onClick={setAnswer}/>
-            <input type="text" disabled number={answer}/>
+            <input type="button" value="=" onClick={handleAnswer}/>
+            <Input type="text" setValue={setAnswer} number={answer}/>
             <Error error={error}/>
         </div>
     )
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Input = ({number, setValue}) => {
 
     return(
-    <div>
         <input onChange={(event)=> setValue(event.target.value)}value={number}/>
-    </div>
 )
 }
 
